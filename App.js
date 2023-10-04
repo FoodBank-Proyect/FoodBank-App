0ee92a87d9b61d92be7e945b3f37b6266e8f0e48
Gerardo Sanchez <gerardosanchezz14@hotmail.com>
2023-10-03 23:30:49 -0600

    Bottom navegation tab added

--- App.js
+++ App.js
@@ -3,9 +3,24 @@ import React, { useEffect } from "react";
 import { SafeAreaView, Switch, Text, View } from "react-native";
 import { useColorScheme } from "nativewind";
 import Navigation from "./navigation";
+import { NavigationContainer } from "@react-navigation/native";
 import { Provider } from "react-redux";
 import store from "./store";
 import AnimatedSplash from "react-native-animated-splash-screen";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import HomeScreen from "./screens/HomeScreen"; // Asegúrate de importar tus componentes de pantalla
+import LoginScreen from "./screens/LoginScreen";
+
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Login Screen" component={LoginScreen} />
+    </Tab.Navigator>
+  );
+}
 
 export default function App() {
   const [isLoaded, setIsLoaded] = React.useState(false);
@@ -26,7 +41,9 @@ export default function App() {
       logoWidth={200}
     >
       <Provider store={store}>
-        <Navigation />
+        <NavigationContainer>
+          <MyTabs />
+        </NavigationContainer>
       </Provider>
     </AnimatedSplash>
   );
