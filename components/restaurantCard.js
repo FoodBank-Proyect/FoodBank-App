c2c329039c840a62be7f3b2e2bbc002b772f0088
CarlosVP120 <carlosveryan4@gmail.com>
2023-10-03 23:01:22 -0600

    biggerCards

--- components/restaurantCard.js
+++ components/restaurantCard.js
@@ -18,9 +18,9 @@ export default function ResturantCard({ item }) {
     >
       <View
         style={{ shadowColor: themeColors.bgColor(0.2), shadowRadius: 7 }}
-        className="mr-6 bg-white rounded-3xl shadow-lg"
+        className="mr-6 bg-white rounded-3xl shadow-lg "
       >
-        <Image className="h-36 w-64 rounded-t-3xl" source={item.image} />
+        <Image className="h-40 w-72 rounded-t-3xl" source={item.image} />
 
         <View className="px-3 pb-4 space-y-2">
           <Text className="text-lg font-bold pt-2">{item.name}</Text>
