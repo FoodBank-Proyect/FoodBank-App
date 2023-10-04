ca90306707898625985d057313eea4d703b52797
Gerardo Sanchez <gerardosanchezz14@hotmail.com>
2023-10-03 21:29:17 -0600

    Cap section implemented

--- components/featuredRestaurants.js
+++ components/featuredRestaurants.js
@@ -1,12 +1,14 @@
 import { View, Text } from "react-native";
 import React from "react";
 import { featured } from "../constants";
+import { utiles } from "../constants";
+import { hogar } from "../constants";
 import FeaturedRow from "./featuredRow";
 
 export default function featuredRestaurants() {
   return (
     <View className="mt-6">
-      {[featured, featured, featured].map((item, index) => {
+      {[featured, utiles, hogar].map((item, index) => {
         return (
           <FeaturedRow
             key={index}
