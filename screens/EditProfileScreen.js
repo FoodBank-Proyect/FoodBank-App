79dcbe83cca7e72d141720e2ffa1d2bf55f6fef4
Gerardo Sanchez <gerardosanchezz14@hotmail.com>
2023-10-06 00:19:24 -0600

    Toalla's and taza's section added; Mi perfil refactored

--- screens/EditProfileScreen.js
+++ screens/EditProfileScreen.js
@@ -5,8 +5,9 @@ import {
   Button,
   TouchableOpacity,
   Dimensions,
+  TextInput,
 } from "react-native";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import * as Icon from "react-native-feather";
 import { useNavigation } from "@react-navigation/native";
 import { auth } from "../firebaseConfig";
@@ -14,6 +15,23 @@ import { Image } from "expo-image";
 
 export default function EditProfileScreen() {
   const halfScreen = Math.round(Dimensions.get("window").height / 1.5);
+  const [newDisplayName, setNewDisplayName] = useState("");
+  const updateDisplayName = () => {
+    if (newDisplayName.trim() !== "") {
+      auth.currentUser
+        .updateProfile({
+          displayName: newDisplayName,
+        })
+        .then(() => {
+          console.log("Nombre de usuario actualizado con éxito");
+          // Puedes realizar otras acciones aquí, como mostrar una notificación de éxito
+        })
+        .catch((error) => {
+          console.error("Error al actualizar el nombre de usuario:", error);
+          // Manejo de errores
+        });
+    }
+  };
 
   const navigation = useNavigation();
   return (
@@ -53,6 +71,9 @@ export default function EditProfileScreen() {
           />
         </TouchableOpacity>
         <Text className="font-bold text-2xl self-center">Mi Perfil</Text>
+        <Text className="text-2xl self-center mt-2">
+          Actualiza tus datos personales
+        </Text>
 
         <View className="flex-col justify-center items-center mt-6">
           {/* User image */}
@@ -67,23 +88,28 @@ export default function EditProfileScreen() {
             />
           </View>
           {/* User name */}
-          <Text className="text-2xl mt-3">Hola,</Text>
-          <Text className="text-2xl font-bold">
-            {auth.currentUser.displayName ||
-              auth.currentUser.email.split("@")[0]}
+          <Text className="text-base mt-4 self-start ml-11 text-gray-300">
+            Nombre de usuario
           </Text>
+
+          {/* Input para el nuevo nombre de usuario */}
+          <TextInput
+            placeholder={
+              auth.currentUser.displayName ||
+              auth.currentUser.email.split("@")[0]
+            }
+            value={newDisplayName}
+            onChangeText={(text) => setNewDisplayName(text)}
+            style={{
+              borderWidth: 0.5,
+              borderColor: "gray",
+              padding: 10,
+              marginTop: 10,
+              width: "80%",
+              borderRadius: 10,
+            }}
+          />
         </View>
-        <TouchableOpacity
-          onPress={() => {
-            // Agrega aquí la lógica para navegar a la pantalla de edición de perfil
-            // Puedes usar navigation.navigate para navegar a la pantalla de edición.
-          }}
-          className="bg-blue-500 rounded-md p-3 w-1/2 mt-5 self-center"
-        >
-          <Text className="text-white text-center font-bold text-lg">
-            Editar perfil
-          </Text>
-        </TouchableOpacity>
 
         {/* Sign Out Button at the bottom */}
         <View className="flex-1 mt-6 items-center">
@@ -91,12 +117,12 @@ export default function EditProfileScreen() {
             onPress={() => {
               auth.signOut();
               auth.currentUser = null;
-              navigation.navigate("Login");
+              navigation.navigate("Login"); // Change to eliminate account
             }}
             className="bg-red-500 rounded-md p-3 w-1/2"
           >
             <Text className="text-white text-center font-bold text-lg">
-              Cerrar sesión
+              Eliminar cuenta
             </Text>
           </TouchableOpacity>
         </View>
