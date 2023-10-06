c53b4716c60790cee92ff2553c291d04459def03
Gerardo Sanchez <gerardosanchezz14@hotmail.com>
2023-10-06 04:35:33 -0600

    Electronicos section added

--- components/featuredRestaurants.js
+++ components/featuredRestaurants.js
@@ -1,14 +1,15 @@
 import { View, Text } from "react-native";
 import React from "react";
-import { featured } from "../constants";
+import { accesorios, featured } from "../constants";
 import { utiles } from "../constants";
 import { hogar } from "../constants";
+import { electronicos } from "../constants";
 import FeaturedRow from "./featuredRow";
 
 export default function featuredRestaurants() {
   return (
     <View className="mt-6">
-      {[featured, utiles, hogar].map((item, index) => {
+      {[featured, utiles, hogar, electronicos].map((item, index) => {
         return (
           <FeaturedRow
             key={index}
